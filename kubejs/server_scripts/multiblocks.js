5f351945730415974b10cb40d00adbea09debc8e
Sep <septechx@proton.me>
2025-12-06 00:55:29 +0100

    refactor: Use builder functions to create recipes for the blast forge

--- kubejs/server_scripts/multiblocks.js
+++ kubejs/server_scripts/multiblocks.js
@@ -24,47 +24,101 @@ MMEvents.createStructures((event) => {
 });
 
 MMEvents.createProcesses((event) => {
-  [
-    "createdieselgenerators:gasoline",
-    "createdieselgenerators:diesel",
-    "tfmg:gasoline",
-    "tfmg:diesel",
-  ].forEach((fuel, i) => {
-    event
-      .create(`mm:steel_${i}`)
-      .structureId("mm:blast_forge")
-      .ticks(400)
-      .input({
-        type: "mm:input/consume",
-        ingredient: {
+  blastForgeRecipes(event)
+    .recipe(
+      "steel_ingot",
+      {
+        type: "mm:item",
+        item: "immersiveengineering:ingot_steel",
+        count: 1,
+      },
+      [
+        {
           type: "mm:item",
           tag: "forge:ingots/iron",
           count: 1,
         },
-      })
-      .input({
-        type: "mm:input/consume",
-        ingredient: {
+        {
           type: "mm:item",
           tag: "forge:dusts/coal_coke",
           count: 1,
         },
-      })
-      .input({
-        type: "mm:input/consume",
-        ingredient: {
-          type: "mm:fluid",
-          fluid: fuel,
-          amount: 150,
-        },
-      })
-      .output({
-        type: "mm:output/simple",
-        ingredient: {
+      ],
+      400,
+    )
+    .recipe(
+      "netherite_ingot",
+      {
+        type: "mm:item",
+        item: "minecraft:netherite_ingot",
+        count: 1,
+      },
+      [
+        {
           type: "mm:item",
-          item: "immersiveengineering:ingot_steel",
-          count: 1,
+          tag: "forge:ingots/netherite_scrap",
+          count: 3,
         },
-      });
-  });
+        {
+          type: "mm:item",
+          tag: "forge:ingots/gold",
+          count: 6,
+        },
+      ],
+      600,
+    )
+    .build();
 });
+
+function blastForgeRecipes(event) {
+  const recipes = [];
+
+  function builder(name, output, inputs, processingTime) {
+    recipes.push((fuel, i) => {
+      let recipe_builder = event
+        .create(`mm:${name}_${i}`)
+        .structureId("mm:blast_forge")
+        .ticks(processingTime)
+        .input({
+          type: "mm:input/consume",
+          ingredient: {
+            type: "mm:fluid",
+            fluid: fuel,
+            amount: 150,
+          },
+        })
+        .output({
+          type: "mm:output/simple",
+          ingredient: output,
+        });
+
+      inputs.forEach((input) => {
+        recipe_builder = recipe_builder.input({
+          type: "mm:input/consume",
+          ingredient: input,
+        });
+      });
+    });
+
+    return {
+      recipe: builder,
+      build: build,
+    };
+  }
+
+  function build() {
+    [
+      "createdieselgenerators:gasoline",
+      "createdieselgenerators:diesel",
+      "tfmg:gasoline",
+      "tfmg:diesel",
+    ].forEach((fuel, i) => {
+      recipes.forEach((recipe) => recipe(fuel, i));
+    });
+  }
+
+  return {
+    recipe: builder,
+    build: build,
+  };
+}
